c114ea50662fbe1cdfb234a12cfa40334d0327fe
Francisco720 <53731816+Francisco720@users.noreply.github.com>
2022-02-18 01:24:45 -0600

    Update Funciones _Error_Flake8.py
    Se corrigieron los errores identificados por Flake8.

--- Tarea 1/Funciones _Error_Flake8.py
+++ Tarea 1/Funciones _Error_Flake8.py
@@ -3,14 +3,17 @@
 # Fecha de última Modificación: 18/2/2022 1:00
 # Versión: 3.10.2
 
-import os
-
 # Funcion: string_work
 # Entradas: Una string
 # Salidas: Una string con cada carácter invertido de mayúscula
 #          a minúscula y viceversa
 # Restricciones: La entrada  de ser una string sin ningún número
-""" Descripción: Verifica que la entrada sea una string sin números y luego se invierte cada carácter de mayúscula a minúscula y viceversa, en caso de no cumplirse lo primero se retorna un código de error."""
+""" Descripción: Verifica que la entrada sea una string sin números
+    y luego se invierte cada carácter de mayúscula a minúscula y
+    viceversa, en caso de no cumplirse lo primero se retorna un
+    código de error."""
+
+
 def string_work(ifstr):
     # Se verifica que la entrada sea una string
     if isinstance(ifstr, str):
